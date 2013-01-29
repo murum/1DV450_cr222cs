dd53e3128fd330395419250a6a3a873da9b7bf03
murum <cr222cs@student.lnu.se>
2013-01-29 12:54:16 +0100

    default rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AMPTA::Application.config.secret_token = '837b70b7b9c1c0d4a0f682a828f1a0019c99e6663a2ba6dce3461c380754d75121c802300cc11d34ab615b82c6ca4191ac8601fb84421416dccac44acd98d252'
